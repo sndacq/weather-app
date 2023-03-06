a3aafa6308a0183597fc492b2b3a76bb8e4bbb50
seandacquel <seandacquel@gmail.com>
2023-03-06 19:56:58 +0800

    [feat] Add Authentication feature using auth0 - added auth0 implementation for authentication - added env variables in next.config.js for auth0 - updated home page with simple login/logout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    AUTH_0_DOMAIN: "yourAuth0AppDomain",
+    AUTH_0_CLIENT: "yorAuth0AppClient",
+  }
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  let redirectUri = '';
+  if (typeof window !== 'undefined') {
+    redirectUri = window.location.origin;
+  }
+
+  return (
+    <Auth0Provider
+      domain={process.env.AUTH_0_DOMAIN || ''}
+      clientId={process.env.AUTH_0_CLIENT || ''}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+      }}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
